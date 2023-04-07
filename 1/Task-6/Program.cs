15cc590152b198af0892b424b1b5fd0cbb249f29
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-07 17:10:09 +0300

    Add Task-6

--- 1/Task-6/Program.cs
+++ 1/Task-6/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите возраст:");
+            double age = Double.Parse(Console.ReadLine());
+            if (age < 1) Console.WriteLine("Младенец");
+            if (age < 11 && age > 1) Console.WriteLine("Ребенок");
+            if (age < 15 && age > 12) Console.WriteLine("Подросток");
+            if (age < 25 && age > 16) Console.WriteLine("Юноша");
+            if (age < 70 && age > 26) Console.WriteLine("Мужчина");
+            if (age < 116  && age > 71) Console.WriteLine("Старик");
+            if (age < double.PositiveInfinity && age > 117) Console.WriteLine("Прах");
         }
     }
 }
