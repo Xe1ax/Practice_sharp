e86890ffc048ea9b5e5d95161368891391f8710e
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-07 13:53:55 +0300

    correcting
    zad-7 change

--- App-A/App-A/zad_7/Program.cs
+++ App-A/App-A/zad_7/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Введите число альфа: ");
             double alfa = Double.Parse(Console.ReadLine());
             Console.WriteLine("z1 = {0}", ((((Math.Sin(4 * alfa))) / (1 + Math.Cos(4 * alfa))) * ((Math.Cos(2 * alfa))/(1 + Math.Cos(2 * alfa)))));
-            Console.WriteLine("z2 = {0}", (1 / Math.Tan((3 / 2) * Math.PI - alfa)));
+            Console.WriteLine("z2 = {0}", ((-1)*(Math.Tan((3 / 2) * Math.PI - alfa))));
         }
     }
 }
