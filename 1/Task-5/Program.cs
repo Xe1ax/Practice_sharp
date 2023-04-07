913774196f36bf2a4c12be8eb7d318d27752aae8
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-07 16:36:32 +0300

    Added Task-5
    Task 5 complete

---
+++ 1/Task-5/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            if (a % 2 == 0) Console.WriteLine("число четное");
+            else Console.WriteLine("число нечетное");
+        }
+    }
+}
