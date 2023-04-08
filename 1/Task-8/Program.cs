5980577dd1a5ea0d54b970a488eaa86f3559d194
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-08 09:16:08 +0300

    little corrections

--- 1/Task-8/Program.cs
+++ 1/Task-8/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите число");
+            Console.Write("Введите число: ");
             int n = int.Parse(Console.ReadLine());
             int sum = 0;
             for (int i = 1; i <= n; i++)
             {
-                sum+= 2*i-1;
+                sum+= 2 * i - 1;
                 Console.WriteLine(sum);
             }
         }
