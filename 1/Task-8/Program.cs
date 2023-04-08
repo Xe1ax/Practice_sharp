99bb2ddf1faaa603226f072676fc86e4f7cab4fd
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-08 09:06:11 +0300

    Add Task-8

---
+++ 1/Task-8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum+= 2*i-1;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
