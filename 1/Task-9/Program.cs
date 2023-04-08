b0344c4192c46f21e603b1364696c3c4253e5daf
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-08 09:39:19 +0300

    Add Task-9

---
+++ 1/Task-9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_9
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                double step = (2 * Math.PI / 3 - Math.PI / 3) / 20;
+                double result;
+                double param = Math.PI / 3;
+                for (int i = 1; i <= 20; i++)
+                {
+                    result =  Math.Cos(param);
+                    Console.WriteLine("f({0:f2}) = {1:f2}", param, result);
+                    param += step;
+                }
+            }
+        }
+    }
