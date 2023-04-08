1c43c4e4cd579d6904dfc2253bfbc398c941b883
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-08 10:38:21 +0300

    Add Task-10
    work complete

---
+++ 1/Task-10/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            for(int i = 10 ; i < 100 ; i++)
+            {
+                a = i / 10;
+                b = i % 10;
+                c = a * a + b * b;
+                if (c % 13 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
