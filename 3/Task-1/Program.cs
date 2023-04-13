b064bf2be5430fe060a625460f1bdc304d10fc09
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-13 23:14:44 +0300

    Task-1 complete

---
+++ 3/Task-1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_1
+{
+         class Program
+        {
+            static double max(double a, double b)
+            {
+                return (a > b) ? a : b;
+            }
+
+            static void Main(string[] args)
+            {
+                Console.Write("Введите x : ");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введите y : ");
+                double y = double.Parse(Console.ReadLine());
+                double z = max(x, 2 * y - x) + max(5 * x + 3 * y, y);
+                Console.WriteLine($"z = {z}");
+            }
+        }
+    } 
+
