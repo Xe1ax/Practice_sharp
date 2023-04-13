62d0b3420a63e1cbe8c547841d7f839e3eb1cbf2
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-13 23:30:14 +0300

    Task-2
    Almost ready

---
+++ 3/Task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_2
+{
+    class Program
+    {
+        static double f(double x, double a)
+        {
+            double y;
+            if (x < a) y = 0;
+            else if (x > a) y = (x - a) / (x + a);
+            else y = 1;
+            return y;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а : ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите b : ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Введите h : ");
+            double h = double.Parse(Console.ReadLine());
+            for (double i = a; i <= b; i += h)
+            Console.WriteLine($"f({i:f2}) = {f(i):f4}") ;
+        }
+    }
+}
