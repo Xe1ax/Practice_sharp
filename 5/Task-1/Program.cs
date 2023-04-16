eaaf0a4d541bede784f5300fdfc062581c589177
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-16 16:56:48 +0300

    Task-1

---
+++ 5/Task-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {0,1,2,3,4,5,6,7,8,9,10};
+            Console.WriteLine(string.Join(", ", arr.Select((x, y) => (x % 2 != 0) ? y + 1 : 0).Where(x => x != 0).ToArray()));
+        }
+    }
+}
