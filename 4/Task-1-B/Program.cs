0d94a97be5ac2037da553588c829f6341ef3deb0
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-16 13:11:10 +0300

    task-1

---
+++ 4/Task-1-B/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_1_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter x: ");
+                double x = double.Parse(Console.ReadLine());
+                double y = Math.Log10(x) + (Math.Cos(x) / x + 6);
+                Console.WriteLine("Sucsess");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: wrong type!");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: divide by 0!");
+            }
+        }
+    }
+}
---
+++ 4/Task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_1_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter x: ");
+                double x = double.Parse(Console.ReadLine());
+                double y = ((Math.Pow(x, 3) - 2) / x + 10) + 4 * x;
+                Console.WriteLine("Sucsess");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: wrong type!");
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Error: divide by 0!");
+            }
+        }
+    }
+}
