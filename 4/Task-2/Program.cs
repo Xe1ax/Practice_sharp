8fe387ba86e5c4e956a062dd024b597848356d0b
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-16 13:46:07 +0300

    Task-2

--- 4/Task-2/Program.cs
+++ 4/Task-2/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.Write("Enter x: ");
+                double x = double.Parse(Console.ReadLine());
+                double f;
+                if ((0 < x) && (x < 1))
+                {
+                    f = 3 * Math.Pow(x, 2);
+                    Console.WriteLine("f(x)=" + f);
+                }
+                else if (x >= 1)
+                {
+                    f = x / x - 1;
+                    Console.WriteLine("f(x)" + f);
+                }
+                else throw new Exception("Error: going out of range");    
+            }
+            catch (Exception fx) 
+            {
+                Console.WriteLine("Error "+fx.Message);
+            }
         }
     }
 }
