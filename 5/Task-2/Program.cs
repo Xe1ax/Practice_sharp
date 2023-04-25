30903509bb190117f088ed7436d48b8d3118b99e
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-25 17:18:30 +0300

    1-Task changes

---
+++ 5/Task-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] arr = {rnd.Next(1,100)};
+            Console.WriteLine(string.Join(", ", arr.OrderBy(n => n >= 0)));
+        }
+    }
+}
