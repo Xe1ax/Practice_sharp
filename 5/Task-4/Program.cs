f0aa121b9b7701ec1dbeae95511fd9033b1791c1
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-28 16:18:38 +0300

    Task-4 and Task-5

---
+++ 5/Task-4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] salaries = new int[18, 12];
+            int totalIncome = 0;
+            int threshold = 0;
+
+            Random rnd = new Random();
+            for (int i = 0; i < 18; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    salaries[i, j] = rnd.Next(1000, 10000);
+                }
+            }
+ 
+            for (int j = 0; j < 12; j++)
+            {
+                totalIncome += salaries[0, j];
+            }
+
+            Console.Write("Введите заданное число: ");
+            threshold = Convert.ToInt32(Console.ReadLine());
+
+            if (totalIncome > threshold)
+            {
+                Console.WriteLine("Годовой доход первого человека больше заданного числа.");
+            }
+            else
+            {
+                Console.WriteLine("Годовой доход первого человека меньше или равен заданному числу.");
+            }
+
+        }
+    }
+}
---
+++ 5/Task-5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double result = CalculateFn(n);
+            Console.WriteLine("f({0}) = {1}", n, result);
+        }
+        static double CalculateFn(int n)
+        {
+            if (n == 0)
+            {
+                return 1.0;
+            }
+            else
+            {
+                double fnMinus1 = CalculateFn(n - 1);
+                return fnMinus1 / (n + 1);
+            }
+        }
+    }
+}
