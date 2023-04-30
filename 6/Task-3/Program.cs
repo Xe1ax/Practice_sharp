906635772a2742a2af859cb4955bd3974e8c28dd
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-04-30 15:55:25 +0300

    Task-3
    Base

--- 6/Task-3/Program.cs
+++ 6/Task-3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите предложение: ");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(' ');
+
+            string firstWord = words[0];
+            string lastWord = words[words.Length - 1];
+
+            string gluedWords = (words[1] += words[2]);
+            Console.WriteLine("Склееные 2 и 3 слова: " + gluedWords);
+
+            char[] thirdWord = words[2].ToCharArray();
+            Array.Reverse(thirdWord);
+            Console.WriteLine("Третье слово в обратном порядке: " + new string(thirdWord));
+
+            string firstTwoLettersRemoved = firstWord.Substring(2);
+            words[0] = firstTwoLettersRemoved;
+            sentence = string.Join(' ', words);
+            Console.WriteLine("Первые две буквы первого слова удалены:" + sentence);
         }
     }
 }
