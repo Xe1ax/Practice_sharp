3f9af77b0bc083fcea700e38519a7894445d06f0
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 00:34:12 +0300

    Task-1

---
+++ 12/Task-1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_1
+{
+    delegate double CalcFigure(double R);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности: ");
+            double radius = double.Parse(Console.ReadLine());
+            CalcFigure CF = new CalcFigure(Get_Length);
+            Console.WriteLine($"Длина окружности с радиусом {radius} = {CF(radius)}");
+
+            CF = new CalcFigure(Get_Area);
+            Console.WriteLine($"Площадь круга с радиусом {radius} = {CF(radius)}");
+
+            CF = new CalcFigure(Get_Volume);
+            Console.WriteLine($"Объем шара с радиусом {radius} = {CF(radius)}");
+        }
+
+        static double Get_Length(double R)
+        {
+            return 2.0 * Math.PI * R;
+        }
+
+        static double Get_Area(double R)
+        {
+            return Math.PI * R * R;
+        }
+
+        static double Get_Volume(double R)
+        {
+            return 4.0 / 3.0 * Math.PI * R * R * R;
+        }
+    }
+}
