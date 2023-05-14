ea1475a34d4857706ee9693fb3e79d4d91dea110
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 17:56:00 +0300

    Task-2 Task-3

---
+++ 13/Task-2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_2
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                var info = new MyInfo();
+                info.NameChanged += name => Console.WriteLine($"Имя изменено на {name}");
+                info.Name = "Иван";
+                info.Name = "Петр";
+            }
+            public class MyInfo
+            {
+                private string _name;
+
+                public event Action<string> NameChanged;
+
+                public string Name
+                {
+                    get { return _name; }
+                    set
+                    {
+                        if (_name != value)
+                        {
+                            _name = value;
+                            OnNameChanged(_name);
+                        }
+                    }
+                }
+
+                protected virtual void OnNameChanged(string newName)
+                {
+                    NameChanged?.Invoke(newName);
+                }
+            }
+        }
+    }
---
+++ 13/Task-3/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task_3
+{
+    public delegate void EventHandler();
+
+    public class EventClass
+    {
+        public event EventHandler MyEvent;
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Do something!");
+            MyEvent?.Invoke();
+        }
+    }
+
+    public class Observer1
+    {
+        public void OnEvent()
+        {
+            Console.WriteLine("Observer 1 reacted to event");
+        }
+    }
+
+    public class Observer2
+    {
+        public void OnEvent()
+        {
+            Console.WriteLine("Observer 2 reacted to event");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventClass eventClass = new EventClass();
+
+            Observer1 observer1 = new Observer1();
+            Observer2 observer2 = new Observer2();
+
+            eventClass.MyEvent += observer1.OnEvent;
+            eventClass.MyEvent += observer1.OnEvent;
+            eventClass.MyEvent += observer2.OnEvent;
+
+            eventClass.DoSomething();
+
+            eventClass.MyEvent -= observer1.OnEvent;
+
+            eventClass.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+}
