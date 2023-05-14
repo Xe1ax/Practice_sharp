621ca14bc0071a1334bb84ab847a6bb288e066dd
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 16:38:53 +0300

    Task-2

--- 10/Task-2/Program.cs
+++ 10/Task-2/Program.cs
@@ -1,10 +1,60 @@
 ﻿namespace Task_2
 {
-    internal class Program
+    class A
+    {
+        private int a = 5;
+        private int b = 10;
+
+        public int C
+        {
+            get { return a + b; }
+        }
+    }
+
+    class B : A
+    {
+        private int d = 15;
+
+        public int D
+        {
+            get { return d; }
+            set { d = value; }
+        }
+
+        public int C2
+        {
+            get
+            {
+                int result = 0;
+                for (int i = 0; i < D; i++)
+                {
+                    result += base.C * i;
+                }
+                return result;
+            }
+        }
+
+        public B() { }
+
+        public B(int d)
+        {
+            this.D = d;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            A a = new A();
+            Console.WriteLine($"Значение свойства С у объекта класса A: {a.C}");
+
+            B b1 = new B();
+            Console.WriteLine($"Значение свойства С у объекта класса B (конструктор 1): {b1.C}");
+            Console.WriteLine($"Значение свойства С2 у объекта класса B (конструктор 1): {b1.C2}");
+
+            B b2 = new B(20);
+            Console.WriteLine($"Значение свойства С2 у объекта класса B (конструктор 2): {b2.C2}");
         }
     }
 }
