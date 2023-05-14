ba32943078d299bd4a911c423dbafe05da9b7c46
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 12:23:10 +0300

    Task-2

--- 12/Task-2/Program.cs
+++ 12/Task-2/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace Task_2
 {
-    internal class Program
+    delegate double CalcDelegate(double a, double b);
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CalcDelegate add = (a, b) => a + b;
+            CalcDelegate sub = (a, b) => a - b;
+            CalcDelegate mul = (a, b) => a * b;
+            CalcDelegate div = (a, b) => b == 0 ? double.NaN : a / b;
+
+            Console.Write("Введите первое число: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Результаты арифметических операций:");
+
+            Console.WriteLine($"Сумма: {add(x, y)}");
+            Console.WriteLine($"Разность: {sub(x, y)}");
+            Console.WriteLine($"Произведение: {mul(x, y)}");
+            Console.WriteLine($"Частное: {div(x, y)}");
+
+            Console.ReadKey();
         }
     }
 }
