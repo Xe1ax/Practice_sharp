122850bc9b7153971aeb0e8173fbc62c158984ab
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 12:34:54 +0300

    Task-3 Task-4

--- 12/Task-3/Program.cs
+++ 12/Task-3/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace Task_3
 {
-    internal class Program
+delegate string StringOperation(string str);
+
+class Program
+{
+    static string ToUpper(string str)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        return str.ToUpper();
+    }
+
+    static string ToLower(string str)
+    {
+        return str.ToLower();
+    }
+
+    static string Reverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    static void Main(string[] args)
+    {
+        StringOperation strOp = new StringOperation(ToUpper);
+        string result = strOp("Hello Pawl!");
+        Console.WriteLine(result);
+
+        strOp = new StringOperation(ToLower);
+        result = strOp("Hello Pawl!");
+        Console.WriteLine(result);
+
+        strOp = new StringOperation(Reverse);
+        result = strOp("Hello Pawl!");
+        Console.WriteLine(result);
         }
     }
 }
--- 12/Task-4/Program.cs
+++ 12/Task-4/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Task_4
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+delegate int MyDelegate();
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+        MyDelegate[] delegatesArray = new MyDelegate[3];
+        delegatesArray[0] = new MyDelegate(() => new Random().Next(1, 10));
+        delegatesArray[1] = new MyDelegate(() => new Random().Next(1, 10));
+        delegatesArray[2] = new MyDelegate(() => new Random().Next(1, 10));
+
+            Func<MyDelegate[], double> average = delegate (MyDelegate[] delegates)
+                {
+                    int sum = 0;
+                    foreach (MyDelegate del in delegates)
+                    {
+                        sum += del.Invoke();
+                    }
+                    return (double)sum / delegates.Length;
+                };
+
+    double result = average(delegatesArray);
+    Console.WriteLine("Среднее арифметическое: {0}", result);
+    Console.ReadKey();
         }
     }
 }
