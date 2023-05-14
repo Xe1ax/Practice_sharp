12049e987a32078dbb9b72fbe3f2010ee44ca1f2
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-14 17:16:43 +0300

    Task-1

---
+++ 13/Task-1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_1
+{
+    delegate string GetColorDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetColorDelegate getColorDelegate = null;
+
+            getColorDelegate += GetRedColor;
+            getColorDelegate += GetGreenColor;
+            getColorDelegate += GetBlueColor;
+
+            string redColor = getColorDelegate();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Red color: " + redColor);         
+
+            string greenColor = getColorDelegate();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Green color: " + greenColor);
+            
+            string blueColor = getColorDelegate();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Blue color: " + blueColor);
+            
+            Console.ReadKey();
+        }
+
+        static string GetRedColor()
+        {
+            return "";
+        }
+
+        static string GetGreenColor()
+        {
+            return "";
+        }
+
+        static string GetBlueColor()
+        {
+            return "";
+        }
+    }
+}
