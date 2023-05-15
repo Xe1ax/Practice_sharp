8509b69c39a5c4500ba79607cf1dd8fd5ee16b2a
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-15 09:15:41 +0300

    Task-3

---
+++ 16/Task-3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = "New_folder";
+            string pathString = Path.Combine(Environment.CurrentDirectory, folderName);
+
+            if (!Directory.Exists(pathString))
+            {
+                Directory.CreateDirectory(pathString);
+                Console.WriteLine($"Папка {pathString} успешно создана.");
+            }
+            else
+            {
+                Console.WriteLine($"Папка {pathString} уже существует.");
+            }
+        }
+    }
+}
