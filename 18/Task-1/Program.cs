785a5ab68db4a825813a7f6c90ec95ed2a0ba869
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-15 12:50:40 +0300

    Task-1,2

---
+++ 18/Task-1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_1
+{
+    class BackspaceString
+    {
+        public static string Process(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in str)
+            {
+                if (c != '#')
+                {
+                    stack.Push(c);
+                }
+                else if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+
+            char[] arr = stack.ToArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abc#d##c";
+            string result = BackspaceString.Process(str);
+            Console.WriteLine("Результат(abc#d##c): " + result); 
+        }
+    }
+}
---
+++ 18/Task-2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"C:\Users\Admin\\Desktop\input.txt");
+
+            Queue<char> charsQueue = new Queue<char>();
+            Queue<char> digitsQueue = new Queue<char>();
+
+            while (!reader.EndOfStream)
+            {
+                char c = (char)reader.Read();
+
+                if (char.IsDigit(c))
+                {
+                    digitsQueue.Enqueue(c);
+                }
+                else
+                {
+                    charsQueue.Enqueue(c);
+                }
+            }
+
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(@"C:\Users\Admin\Desktop\output.txt");
+
+            while (charsQueue.Count > 0)
+            {
+                writer.Write(charsQueue.Dequeue());
+            }
+
+            while (digitsQueue.Count > 0)
+            {
+                writer.Write(digitsQueue.Dequeue());
+            }
+
+            writer.Close();
+        }
+    }
+}
