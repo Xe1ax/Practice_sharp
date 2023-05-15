4bbd151f7ef163db34f52cad5dd12e5fe1cedef1
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-15 09:41:35 +0300

    Task-3,4

---
+++ 16/Task-4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"C:\\Users\\Admin\\Desktop\\input.txt";  
+            string outputFilePath = @"C:\\Users\\Admin\\Desktop\\output.txt";  
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string replacedLine = ReplaceZerosAndOnes(line);
+                    writer.WriteLine(replacedLine);
+                }
+            }
+        }
+
+        static string ReplaceZerosAndOnes(string line)
+        {
+            char[] chars = line.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '0')
+                {
+                    chars[i] = '1';
+                }
+                else if (chars[i] == '1')
+                {
+                    chars[i] = '0';
+                }
+            }
+
+            return new string(chars);
+        }
+    }
+}
