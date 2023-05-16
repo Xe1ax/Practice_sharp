9cfd1246b14192c6894b93cbc8dfedba1efe25e5
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-16 11:02:11 +0300

    Task-4

---
+++ 20/Task-4/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 54, 17, 25, 66 };
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            try
+            {
+                Parallel.ForEach(numbers, new ParallelOptions { CancellationToken = cts.Token }, (number) =>
+                {
+                    int result = Sum(number);
+                    Console.WriteLine($"Сумма чисел от 0 до {number} = {result}");
+
+                    if (result > 100)
+                    {
+                        cts.Cancel();
+                    }
+                });
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Выполнение было прервано.");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Sum(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+
+        static long Product(int n)
+        {
+            long result = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
