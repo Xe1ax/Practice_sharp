a310dff3024c2d61bbbf661689725c1f0f9954be
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-16 10:58:56 +0300

    Task-3

--- 20/Task-3/Program.cs
+++ 20/Task-3/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Task_3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double a = -7;
+            double b = 3;
+            int n = 1000;
+            double[] results = new double[n];
+
+            Parallel.For(0, n, i =>
+            {
+                double x = a + (b - a) * i / (n - 1);
+                results[i] = Math.Cos(x);
+            });
+
+            Console.WriteLine("Результаты:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Cos({0:F2}) = {1:F4}", a + (b - a) * i / (n - 1), results[i]);
+            }
         }
     }
 }
