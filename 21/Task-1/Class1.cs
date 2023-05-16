bf1ef34ed3bc602580fda2719e49f3e0d7c478dd
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-16 11:10:55 +0300

    Task-1

---
+++ 21/Task-1/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_1;
+
+namespace Task_1
+{
+    public interface IFlyBehavior
+    {
+        void Fly();
+    }
+
+    public interface IQuackBehavior
+    {
+        void Quacki();
+    }
+}
+public class FlyWithWings : IFlyBehavior
+{
+    public void Fly()
+    {
+        Console.WriteLine("I'm flying with wings!");
+    }
+}
+
+public class FlyNoWay : IFlyBehavior
+{
+    public void Fly()
+    {
+        Console.WriteLine("I can't fly.");
+    }
+}
+public class Quack : IQuackBehavior
+{
+    public void Quacki()
+    {
+        Console.WriteLine("Quack quack!");
+    }
+}
+
+public class Squeak : IQuackBehavior
+{
+    public void Quacki()
+    {
+        Console.WriteLine("Squeak squeak!");
+    }
+}
+
+public class MuteQuack : IQuackBehavior
+{
+    public void Quacki()
+    {
+        Console.WriteLine("<< Silence >>");
+    }
+}
+public class Duck
+{
+    private IFlyBehavior flyBehavior;
+    private IQuackBehavior quackBehavior;
+
+    public Duck(IFlyBehavior flyBehavior, IQuackBehavior quackBehavior)
+    {
+        this.flyBehavior = flyBehavior;
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void SetFlyBehavior(IFlyBehavior flyBehavior)
+    {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void SetQuackBehavior(IQuackBehavior quackBehavior)
+    {
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void PerformFly()
+    {
+        flyBehavior.Fly();
+    }
+
+    public void PerformQuack()
+    {
+        quackBehavior.Quacki();
+    }
+}
---
+++ 21/Task-1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Duck mallardDuck = new Duck(new FlyWithWings(), new Quack());
+            mallardDuck.PerformFly(); // I'm flying with wings!
+            mallardDuck.PerformQuack(); // Quack quack!
+            mallardDuck.SetFlyBehavior(new FlyNoWay());
+            mallardDuck.PerformFly(); // I can't fly.
+            mallardDuck.SetQuackBehavior(new Squeak());
+            mallardDuck.PerformQuack(); // Squeak squeak!
+        }
+    }
+}
