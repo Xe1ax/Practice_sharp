b5bf8027432aaf69df5ab7e33d7640de18b11ecd
Xe1ax <127730527+Xe1ax@users.noreply.github.com>
2023-05-16 10:56:01 +0300

    Task-2

---
+++ 20/Task-2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task1 = Task.Run(() =>
+            {
+                int number = 345;
+                int firstDigit = number / 100;
+                int lastDigit = number % 10;
+                return int.Parse($"{lastDigit}{number % 100 / 10}{firstDigit}");
+            });
+
+            Task task2 = task1.ContinueWith(previousTask =>
+            {
+                Console.WriteLine("Результат первой задачи: " + previousTask.Result);
+            });
+
+            Task.WaitAll(task1, task2);
+        }
+    }
+}
